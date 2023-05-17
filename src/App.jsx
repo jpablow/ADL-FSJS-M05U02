db636c2bad9d080d6e0f80282c243aaa07534de7
Juan Pablo Villanueva <juanpablow@gmail.com>
2023-05-16 21:03:50 -0400

    Crea Contexto

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,35 @@
-import "./styles.css";
+import './styles.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Navbar from "./components/Navbar";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
 
-import Home from "./views/Home";
-import Favoritos from "./views/Favoritos";
+import Home from './views/Home';
+import Favoritos from './views/Favoritos';
+import { useEffect, useState } from 'react';
 
 export default function App() {
-  const endpoint = "/fotos.json";
+  const url = './src/fotos.json';
+  const [photos, setPhotos] = useState([]);
+
+  const getData = async () => {
+    try {
+      const res = await fetch(url);
+      const data = await res.json();
+      setPhotos([data.photos]);
+    } catch (error) {
+      alert(
+        'Se ha producido un error al recuperar los datos => ' +
+          error.name +
+          ': ' +
+          error.message
+      );
+    }
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
 
   return (
     <div className="App">
---
+++ src/ElContexto.jsx
@@ -0,0 +1,3 @@
+import { createContext } from 'react';
+const ElContexto = createContext({});
+export default ElContexto;
