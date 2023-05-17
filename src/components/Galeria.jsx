eb45334385e743e808875dd410b1be0b126f1e43
Juan Pablo Villanueva <juanpablow@gmail.com>
2023-05-17 01:03:56 -0400

    Modifica código de Galería y CSS de navbar

--- src/components/Galeria.jsx
+++ src/components/Galeria.jsx
@@ -1,14 +1,10 @@
 import '../assets/css/galeria.css';
 import Heart from './Heart';
 import ElContexto from '../ElContexto';
-import { useContext, useEffect } from 'react';
+import { useContext } from 'react';
 
 export default function Home() {
   const { photos } = useContext(ElContexto);
-
-  useEffect(() => {
-    console.log(photos);
-  }, []);
 
   return (
     <div className="galeria grid-columns-4 p-3">
